908d9eb733c2e9f8928ceaacb8b5cb536160974f
Kiri Choi <kirichoi@uw.edu>
2018-10-17 15:12:27 -0700

    Analyze modifiers/set figure size automatically

--- main.py
+++ main.py
@@ -8,10 +8,15 @@ Kiri Choi (c) 2018
 
 import tellurium as te
 import antimony
+import libsbml
 import networkx as nx
 from matplotlib.patches import FancyArrowPatch, Rectangle
 import matplotlib.pyplot as plt
 import numpy as np
+import sympy
+import test_models as ts
+import tesbml
+import re
 
 def plotNetworkFromSBML(model):
     """
@@ -36,36 +41,99 @@ def plotNetwork(model, lw=3):
     numBnd = r.getNumBoundarySpecies()
     numFlt = r.getNumFloatingSpecies()
     
-    antimony.loadAntimonyString(model)
-    module = antimony.getModuleNames()[-1]
-    rct = np.array(antimony.getReactantNames(module)).tolist()
-    prd = np.array(antimony.getProductNames(module)).tolist()
+    paramIdsStr = ' '.join(r.getGlobalParameterIds())
+    floatingIdsStr = ' '.join(r.getFloatingSpeciesIds())
+    boundaryIdsStr = ' '.join(r.getBoundarySpeciesIds())
+    comparmentIdsStr = ' '.join(r.getCompartmentIds())
+    
+    allIds = paramIdsStr + ' ' + floatingIdsStr + ' ' + boundaryIdsStr + ' ' + comparmentIdsStr
+    
+    avsym = sympy.symbols(allIds)
+    
+    rct = []
+    prd = []
+    mod = []
+    kineticLaw = []
+    mod_type = []
+    
+    doc = tesbml.readSBMLFromString(r.getSBML())
+    sbmlmodel = doc.getModel()
+
+    for slr in sbmlmodel.getListOfReactions():
+        temprct = []
+        tempprd = []
+        tempmod = []
+        
+        sbmlreaction = sbmlmodel.getReaction(slr.getId())
+        for sr in range(sbmlreaction.getNumReactants()):
+            sbmlrct = sbmlreaction.getReactant(sr)
+            temprct.append(sbmlrct.getSpecies())
+        for sp in range(sbmlreaction.getNumProducts()):
+            sbmlprd = sbmlreaction.getProduct(sp)
+            tempprd.append(sbmlprd.getSpecies())
+        for sm in range(sbmlreaction.getNumModifiers()):
+            sbmlmod = sbmlreaction.getModifier(sm)
+            tempmod.append(sbmlmod.getSpecies())
+        kl = sbmlreaction.getKineticLaw()
+        
+        rct.append(temprct)
+        prd.append(tempprd)
+        mod.append(tempmod)
+        kineticLaw.append(kl.getFormula())
+    
+    # Sympy weSmart
+    for ml in range(len(mod)):
+        mod_type_temp = []
+        expression = kineticLaw[ml]
+        n,d = sympy.fraction(expression)
+        for ml_i in range(len(mod[ml])):
+            if n.has(mod[ml][ml_i]):
+                mod_type_temp.append('activator')
+            elif d.has(mod[ml][ml_i]):
+                mod_type_temp.append('inhibitor')
+            else:
+                continue
+        mod_type.append(mod_type_temp)
     
     G = nx.DiGraph()
 
-    for i in range(antimony.getNumReactions(module)):
+    # TODO: Support modifiers
+    # TODO: Separate boundary species?
+    for i in range(sbmlmodel.getNumReactions()):
         if len(rct[i]) == 1:
             if len(prd[i]) == 1:
                 G.add_edges_from([(rct[i][0], prd[i][0])], weight=(1+lw))
             else:
-                G.add_edges_from([(rct[i][0], 'J')], weight=(1+lw))
+                G.add_edges_from([(rct[i][0], '')], weight=(1+lw))
                 for j in range(len(prd[i])):
-                    G.add_edges_from([('J', prd[i][j])], weight=(1+lw))
+                    G.add_edges_from([('', prd[i][j])], weight=(1+lw))
         else:
             if len(prd[i]) == 1:
                 for k in range(len(rct[i])):
-                    G.add_edges_from([(rct[i][k], 'J')], weight=(1+lw))
-                G.add_edges_from([('J', prd[i][0])], weight=(1+lw))
+                    G.add_edges_from([(rct[i][k], '')], weight=(1+lw))
+                G.add_edges_from([('', prd[i][0])], weight=(1+lw))
             else:
                 for k in range(len(rct[i])):
-                    G.add_edges_from([(rct[i][k], 'J1')], weight=(1+lw))
-                G.add_edges_from([("J1", 'J2')], weight=(1+lw))
+                    G.add_edges_from([(rct[i][k], '')], weight=(1+lw))
                 for j in range(len(prd[i])):
-                    G.add_edges_from([('J2', prd[i][j])], weight=(1+lw))
+                    G.add_edges_from([('', prd[i][j])], weight=(1+lw))
+                    
+        if len(mod[i]) > 0:
+            if mod_type[i][0] == 'inhibitor':
+                G.add_edges_from([(mod[i][0], rct[i][0])], weight=(1+lw))
+            elif mod_type[i][0] == 'activator':
+                G.add_edges_from([(mod[i][0], rct[i][0])], weight=(1+lw))
     
     pos = nx.spring_layout(G)
     
-    # TODO: Figure out the maximum position
+    max_width = []
+    max_height = []
+    for key, value in pos.items():
+        max_width.append(value[0])
+        max_height.append(value[1])
+    
+    max_width = [min(max_width), max(max_width)]
+    max_height = [min(max_height), max(max_height)]
     
 #    edges = G.edges()
 #    weights = [G[u][v]['weight'] for u,v in edges]
@@ -77,13 +145,17 @@ def plotNetwork(model, lw=3):
 #    nx.draw(G, pos, node_size=250*strMaxLen, with_labels=True, width=weights, 
 #            node_shape='s')
     
+    
     fontsize = 20
     
     fig = plt.figure()
     ax = plt.gca()
     
     for n in G:
-        c = Rectangle(pos[n], width=0.1*(strMaxLen+strMaxLen*0.1), height=0.15)
+        if n == '':
+            c = Rectangle(pos[n], width=0, height=0)
+        else:
+            c = Rectangle(pos[n], width=max(0.05*strMaxLen, 0.2), height=0.15)
         ax.add_patch(c)
         G.node[n]['patch'] = c
         plt.text(pos[n][0]+c.get_width()/2, pos[n][1]+c.get_height()/2, n, fontsize=fontsize, 
@@ -94,13 +166,13 @@ def plotNetwork(model, lw=3):
         n2 = G.node[v]['patch']
         rad = 0.1
         if (u,v) in seen:
-            rad = seen.get((u,v))
-            rad = (rad+np.sign(rad)*0.1)*-1
+            rad = seen.get((u,v)) # TODO: No curvature when there is just a single line between two nodes
+            rad = (rad+np.sign(rad)*0.1)*-1 # TODO: Change curvature
         color = 'k'
 
         e = FancyArrowPatch((n1.xy[0]+n1.get_width()/2,n1.xy[1]+n1.get_height()/2),
                             (n2.xy[0]+n2.get_width()/2,n2.xy[1]+n2.get_height()/2),
-                            patchA=n1, 
+                            patchA=n1,
                             patchB=n2,
                             arrowstyle='-|>',
                             connectionstyle='arc3,rad=%s'%rad,
@@ -111,15 +183,14 @@ def plotNetwork(model, lw=3):
         ax.add_patch(e)
     
     ax.autoscale()
-    fig.set_figheight(10)
-    fig.set_figwidth(20)
+    fig.set_figwidth((abs(max_width[0] - max_width[1])+0.5)*5)
+    fig.set_figheight((abs(max_height[0] - max_height[1])+0.5)*5)
     plt.axis('off')
     plt.axis('equal')
     
     
     plt.show()
     
-    antimony.clearPreviousLoads()
     
 def plotWeightedNetwork(models, lw=10):
     """     
@@ -172,10 +243,19 @@ def plotWeightedNetwork(models, lw=10):
                 for j in range(len(allRxn[i][1])):
                     G.add_edges_from([('J', allRxn[i][1][j])], weight=count[i]*lw)
 
-    pos = nx.kamada_kawai_layout(G)
+    pos = nx.spring_layout(G)
     
-    edges = G.edges()
-    weights = [G[u][v]['weight'] for u,v in edges]
+    max_width = []
+    max_height = []
+    for key, value in pos.items():
+        max_width.append(value[0])
+        max_height.append(value[1])
+    
+    max_width = [min(max_width), max(max_width)]
+    max_height = [min(max_height), max(max_height)]
+    
+#    edges = G.edges()
+#    weights = [G[u][v]['weight'] for u,v in edges]
     
     edgeLabels = {}
     for i in range(len(allRxn)):
@@ -197,8 +277,59 @@ def plotWeightedNetwork(models, lw=10):
                 for j in range(len(allRxn[i][1])):
                     edgeLabels[('J', allRxn[i][1][j])] = round(count[i], 3)
     
-    nx.draw(G, pos, node_size=500, with_labels=True, width=weights)
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edgeLabels, label_pos=0.3)
+    flat_rct = [item for sublist in rct for item in sublist]
+    flat_prd = [item for sublist in prd for item in sublist]
+
+    strMaxLen = len(max(flat_rct+flat_prd, key=len))
+#    nx.draw(G, pos, node_size=250*strMaxLen, with_labels=True, width=weights, 
+#            node_shape='s')
+    
+    
+    fontsize = 20
+    
+    fig = plt.figure()
+    ax = plt.gca()
+    
+    for n in G:
+        if n == '':
+            c = Rectangle(pos[n], width=0, height=0)
+        else:
+            c = Rectangle(pos[n], width=max(0.05*strMaxLen, 0.2), height=0.15)
+        ax.add_patch(c)
+        G.node[n]['patch'] = c
+        plt.text(pos[n][0]+c.get_width()/2, pos[n][1]+c.get_height()/2, n, fontsize=fontsize, 
+                 horizontalalignment='center', verticalalignment='center')
+    seen={}
+    for (u,v,d) in G.edges(data=True):
+        n1 = G.node[u]['patch']
+        n2 = G.node[v]['patch']
+        rad = 0.1
+        if (u,v) in seen:
+            rad = seen.get((u,v))
+            rad = (rad+np.sign(rad)*0.1)*-1 # TODO: Change curvature
+        color = 'k'
+
+        e = FancyArrowPatch((n1.xy[0]+n1.get_width()/2,n1.xy[1]+n1.get_height()/2),
+                            (n2.xy[0]+n2.get_width()/2,n2.xy[1]+n2.get_height()/2),
+                            patchA=n1,
+                            patchB=n2,
+                            arrowstyle='-|>',
+                            connectionstyle='arc3,rad=%s'%rad,
+                            mutation_scale=10.0,
+                            lw=G[u][v]['weight'],
+                            color=color)
+        seen[(u,v)]=rad
+        ax.add_patch(e)
+    
+    ax.autoscale()
+    fig.set_figwidth((abs(max_width[0] - max_width[1])+0.5)*5)
+    fig.set_figheight((abs(max_height[0] - max_height[1])+0.5)*5)
+    plt.axis('off')
+    plt.axis('equal')
+    
+    
+#    nx.draw(G, pos, node_size=500, with_labels=True, width=weights)
+#    nx.draw_networkx_edge_labels(G, pos, edge_labels=edgeLabels, label_pos=0.3)
     
     plt.show()
     
